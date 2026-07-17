ec2a670948cc6dcda0f0992586ecbc54d7596c5d
chalchitradarpan <90804287+chalchitradarpan@users.noreply.github.com>
2026-07-17 18:21:47 +0530

    Update TheTeam_2k25.js

--- src/TheTeam_2k25.js
+++ src/TheTeam_2k25.js
@@ -44,7 +44,7 @@ const TheTeam2k25 = () => {
 				</Grid>
 				<Grid item xs={12} md={6} lg={4} className={styles.person}>
 					<span className={styles.image__block} >
-						<img className={styles.image} src="dummy_image.jpg" alt="Niveditha"></img>
+						<img className={styles.image} src="niveditha_editor.jpeg" alt="Niveditha"></img>
 					</span>
 					<span className={styles.name}>
 						Niveditha
